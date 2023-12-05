98bbd79bcb2a4d5d61b51ddbea92b5add161b9a0
Thawfeek Yahya <thawfeek_yahya@yahoo.co.in>
2023-12-05 19:30:01 +0530

    conditional_statements

---
+++ chapter1/conditional.py
@@ -0,0 +1,24 @@
+# Conditional Statements in python
+
+x=int(input("Enter a number between 0 and 9"))
+if x == 1: print("You have entered 1")
+elif x == 2: print("You have entered 2")
+elif x == 3: print("You have entered 3")
+elif x == 4: print("You have entered 4")
+elif x == 5: print("You have entered 5")
+elif x == 6: print("You have entered 6")
+elif x == 7: print("You have entered 7")
+elif x == 8: print("You have entered 8")
+elif x == 9: print("You have entered 9")
+elif x == 0: print("You have entered 0")
+
+
+# Statements in suite
+if x == 2:
+    print("The value you entered")
+    print("is the number")
+    print("Two")
+else:
+    print("The value you entered")
+    print("is not the number")
+    print("Two")
